91f7c52c9b558f0d1fff8d3077a9d2aab8b9cd27
Kevin Raimbaud <kevin.raimbaud@ext.hermes.com>
2023-08-23 20:36:31 +0200

    Using modify extension

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 import { walk } from "https://deno.land/std@0.199.0/fs/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 
-const renameFiles = async ({ path: directoryPath, prefix, suffix }: { path: string, prefix?: string, suffix?: string }) => {
+const renameFiles = async ({
+   path: directoryPath,
+   prefix,
+   suffix,
+   extension
+}: {
+    path: string;
+    prefix?: string;
+    suffix?: string;
+    extension?: string;
+}) => {
     let counter = 1;
     const tempFiles: string[] = [];
 
@@ -11,8 +21,7 @@ const renameFiles = async ({ path: directoryPath, prefix, suffix }: { path: stri
         // If the file has no extension, skip it.
         if (lastDotIndex === -1) continue;
 
-        const extension = entry.path.substring(lastDotIndex);
-        const tempName = `${self.crypto.randomUUID()}${counter}${extension}`;
+        const tempName = `${self.crypto.randomUUID()}${counter}${extension || entry.path.substring(lastDotIndex)}`;
         tempFiles.push(tempName);
 
         await Deno.rename(entry.path, `${directoryPath}/${tempName}`);
@@ -21,8 +30,8 @@ const renameFiles = async ({ path: directoryPath, prefix, suffix }: { path: stri
 
     counter = 1;
     for (const tempFile of tempFiles) {
-        const extension = tempFile.substring(tempFile.lastIndexOf("."));
-        const newName = `${prefix || ""}${counter}${suffix || ""}${extension}`;
+        const currentExtension = extension ? `.${extension}` : tempFile.substring(tempFile.lastIndexOf("."));
+        const newName = `${prefix || ""}${counter}${suffix || ""}${currentExtension}`;
         await Deno.rename(`${directoryPath}/${tempFile}`, `${directoryPath}/${newName}`);
         counter++;
     }
@@ -31,11 +40,13 @@ const renameFiles = async ({ path: directoryPath, prefix, suffix }: { path: stri
 const cli = new Command()
     .name("rename-files")
     .description("Rename files in the given directory.")
-    .option("-p, --path <path:string>", "Path to the directory containing files to be renamed.")
-    .option("-x, --prefix [prefix:string]", "Optional prefix to add to the file names.")
+    .option("-a, --path <path:string>", "Path to the directory containing files to be renamed.")
+    .option("-p, --prefix [prefix:string]", "Optional prefix to add to the file names.")
     .option("-s, --suffix [suffix:string]", "Optional suffix to add to the file names before the extension.")
+    .option("-e, --extension [extension:string]", "Optional new extension for the files (without the dot).")
     .action(renameFiles);
 
 if (import.meta.main) {
     await cli.parse(Deno.args);
 }
+
