e0e31cdec3d69386af9ad730bdba7a98a5fd5018
Kevin Raimbaud <kevin.raimbaud@ext.hermes.com>
2023-08-23 21:11:51 +0200

    add iteration

--- main.ts
+++ main.ts
@@ -3,17 +3,19 @@ import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 import { red, green } from "https://deno.land/std@0.199.0/fmt/colors.ts";
 
 const renameFiles = async ({
-   path: directoryPath,
-   prefix,
-   suffix,
-   extension
-}: {
+                               path: directoryPath,
+                               prefix,
+                               suffix,
+                               extension,
+                               iteration
+                           }: {
     path: string;
     prefix?: string;
     suffix?: string;
     extension?: string;
+    iteration?: number;
 }) => {
-    let counter = 1;
+    let counter = iteration || 1;  // Use the given iteration value or default to 1
     const tempFiles: { oldName: string; tempName: string }[] = [];
 
     for await (const entry of walk(directoryPath, { maxDepth: 1, includeDirs: false })) {
@@ -30,7 +32,7 @@ const renameFiles = async ({
         counter++;
     }
 
-    counter = 1;
+    counter = iteration || 1;
     for (const { oldName, tempName } of tempFiles) {
         const currentExtension = extension ? `.${extension}` : tempName.substring(tempName.lastIndexOf("."));
         const newName = `${prefix || ""}${counter}${suffix || ""}${currentExtension}`;
@@ -49,6 +51,7 @@ const cli = new Command()
     .option("-p, --prefix [prefix:string]", "Optional prefix to add to the file names.")
     .option("-s, --suffix [suffix:string]", "Optional suffix to add to the file names before the extension.")
     .option("-e, --extension [extension:string]", "Optional new extension for the files (without the dot).")
+    .option("-i, --iteration [iteration:number]", "Starting number for iteration (default is 1).")
     .action(renameFiles);
 
 if (import.meta.main) {
