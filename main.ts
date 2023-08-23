00e9530c70736bd97eb76e1d8be48cfbeb8eb537
Kevin Raimbaud <kevin.raimbaud@ext.hermes.com>
2023-08-23 20:22:24 +0200

    add uuid

--- main.ts
+++ main.ts
@@ -3,16 +3,27 @@ import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 
 const renameFiles = async ({ path: directoryPath, prefix, suffix }: { path: string, prefix?: string, suffix?: string }) => {
     let counter = 1;
+    const tempFiles: string[] = [];
 
     for await (const entry of walk(directoryPath, { maxDepth: 1, includeDirs: false })) {
-        const extension = entry.path.substring(entry.path.lastIndexOf("."));
+        const lastDotIndex = entry.path.lastIndexOf(".");
+
+        // If the file has no extension, skip it.
+        if (lastDotIndex === -1) continue;
+
+        const extension = entry.path.substring(lastDotIndex);
         const tempName = `${self.crypto.randomUUID()}${counter}${extension}`;
+        tempFiles.push(tempName);
 
         await Deno.rename(entry.path, `${directoryPath}/${tempName}`);
+        counter++;
+    }
 
+    counter = 1;
+    for (const tempFile of tempFiles) {
+        const extension = tempFile.substring(tempFile.lastIndexOf("."));
         const newName = `${prefix || ""}${counter}${suffix || ""}${extension}`;
-        await Deno.rename(`${directoryPath}/${tempName}`, `${directoryPath}/${newName}`);
-
+        await Deno.rename(`${directoryPath}/${tempFile}`, `${directoryPath}/${newName}`);
         counter++;
     }
 }
